e0c1bba3afd77cb597993b60b320811c389d5d80
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-07-15 13:50:24 +0900

    bunlde gem

---
+++ lib/ovto/router.rb
@@ -0,0 +1,8 @@
+require "ovto/router/version"
+
+module Ovto
+  module Router
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ovto/router/version.rb
@@ -0,0 +1,5 @@
+module Ovto
+  module Router
+    VERSION = "0.1.0"
+  end
+end
